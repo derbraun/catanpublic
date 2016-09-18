42a807e63fddef009f08b2d54dc9b21b2b7b418d
Rocker398 <paden19@hotmail.com>
2016-09-18 08:40:42 -0600

    shared server stuff (IServer)

---
+++ java/src/shared/ClientServerException.java
@@ -0,0 +1,22 @@
+package shared;
+
+@SuppressWarnings("serial")
+public class ClientServerException
+	extends Exception
+{
+	public ClientServerException() {
+		super();
+	}
+
+	public ClientServerException(String message) {
+		super(message);
+	}
+
+	public ClientServerException(Throwable throwable) {
+		super(throwable);
+	}
+
+	public ClientServerException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
